c624d61003c7ef7d833b71046502e3f1a279fcbb
C.J. Jameson <cjameson@tableau.com>
2020-09-27 21:42:20 -0700

    hammerspoon shift it yourself manually

--- brew-install.sh
+++ brew-install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
-brew cask install hammerspoon
+
 brew cask install flycut
 
+#shiftit
+brew cask install hammerspoon
+
+echo "do the accessibility things for flycut and hammerspoon"
+echo "install shiftit spoon https://github.com/peterklijn/hammerspoon-shiftit#installation"
