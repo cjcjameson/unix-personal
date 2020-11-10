1f9b1afa44c349b376e962b004f683db0fe39401
C.J. Jameson <cjameson@tableau.com>
2020-11-10 13:39:27 -0800

    git config to use ssh instead of https
    https://medium.com/easyread/today-i-learned-fix-go-get-private-repository-return-error-terminal-prompts-disabled-8c5549d89045
    
    https://stackoverflow.com/questions/32232655/go-get-results-in-terminal-prompts-disabled-error-for-github-private-repo

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
 git config --global alias.co checkout
+
+git config --global url."git@gitlab.com:".insteadOf "https://gitlab.com/"
+git config --global url."git@gitlab.tableausoftware.com:".insteadOf "https://gitlab.tableausoftware.com/"
